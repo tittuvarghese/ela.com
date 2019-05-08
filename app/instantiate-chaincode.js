e791fd94f3931257074d32c7288750116ed276f5
tittuvarghese <2311294+tittuvarghese@users.noreply.github.com>
2019-05-08 16:19:39 +0530

    Minor fix

--- app/instantiate-chaincode.js
+++ app/instantiate-chaincode.js
@@ -1,83 +1,67 @@
-/**
- * Copyright 2017 IBM All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 'use strict';
-var util = require('util');
-var helper = require('./helper.js');
-var logger = helper.getLogger('instantiate-chaincode');
 
-var instantiateChaincode = async function(peers, channelName, chaincodeName, chaincodeVersion, functionName, chaincodeType, args, username, org_name) {
-	logger.debug('\n\n============ Instantiate chaincode on channel ' + channelName +
-		' ============\n');
-	var error_message = null;
+const CONSTANTS = require('../constants.js');
+let util = require('util');
+let helper = require('./helper.js');
+let logger = helper.getLogger('instantiate-chaincode');
+
+/**
+* installChaincode - function to install chaincode on peers
+* @params peers {array}, channelName, chaincodeName, chaincodeVersion, chaincodeType
+* @params functionName {init}, chaincodeType, username, org_name
+* @returns response {JSON_Array}
+*/
+let instantiateChaincode = async function(peers, channelName, chaincodeName, chaincodeVersion, chaincodeType, functionName, username, org_name) {
+	logger.debug('\n\n============ Instantiate chaincode on channel ' + channelName + ' ============\n');
+	let error_message = null;
 
 	try {
 		// first setup the client for this org
-		var client = await helper.getClientForOrg(org_name, username);
+		let client = await helper.getClientForOrg(org_name, username);
 		logger.debug('Successfully got the fabric client for the organization "%s"', org_name);
-		var channel = client.getChannel(channelName);
+		let channel = client.getChannel(channelName);
 		if(!channel) {
 			let message = util.format('Channel %s was not defined in the connection profile', channelName);
 			logger.error(message);
-			throw new Error(message);
+			let response = {
+				success: false,
+				message: 'Invalid Channel name for this network configuration.'
+			}
+			return response;
 		}
-		var tx_id = client.newTransactionID(true); // Get an admin based transactionID
+		let tx_id = client.newTransactionID(true); // Get an admin based transactionID
 		                                       // An admin based transactionID will
 		                                       // indicate that admin identity should
 		                                       // be used to sign the proposal request.
 		// will need the transaction ID string for the event registration later
-		var deployId = tx_id.getTransactionID();
+		let deployId = tx_id.getTransactionID();
 
 		// send proposal to endorser
-		var request = {
+		let request = {
 			targets : peers,
 			chaincodeId: chaincodeName,
 			chaincodeType: chaincodeType,
 			chaincodeVersion: chaincodeVersion,
-			args: args,
+			fcn: functionName,
 			txId: tx_id,
-
-			// Use this to demonstrate the following policy:
-			// The policy can be fulfilled when members from both orgs signed.
-			'endorsement-policy': {
-			        identities: [
-					{ role: { name: 'member', mspId: 'Org1MSP' }},
-					{ role: { name: 'member', mspId: 'Org2MSP' }}
-			        ],
-			        policy: {
-					'2-of':[{ 'signed-by': 0 }, { 'signed-by': 1 }]
-			        }
-		        }
+			'collections-config': __dirname + '/endorsement/collections_config.json',
 		};
 
-		if (functionName)
-			request.fcn = functionName;
-
-		let results = await channel.sendInstantiateProposal(request, 60000); //instantiate takes much longer
+		let results = await channel.sendInstantiateProposal(request, CONSTANTS.INSTANTIATION_TIMEOUT);
+		//instantiate takes much longer
 
 		// the returned object has both the endorsement results
 		// and the actual proposal, the proposal will be needed
 		// later when we send a transaction to the orderer
-		var proposalResponses = results[0];
-		var proposal = results[1];
+		let proposalResponses = results[0];
+		let proposal = results[1];
+		console.log(proposalResponses);
 
 		// lets have a look at the responses to see if they are
 		// all good, if good they will also include signatures
 		// required to be committed
-		var all_good = true;
-		for (var i in proposalResponses) {
+		let all_good = true;
+		for (let i in proposalResponses) {
 			let one_good = false;
 			if (proposalResponses && proposalResponses[i].response &&
 				proposalResponses[i].response.status === 200) {
@@ -90,14 +74,14 @@ var instantiateChaincode = async function(peers, channelName, chaincodeName, cha
 		}
 
 		if (all_good) {
-			logger.info(util.format(
+			 logger.info(util.format(
 				'Successfully sent Proposal and received ProposalResponse: Status - %s, message - "%s", metadata - "%s", endorsement signature: %s',
 				proposalResponses[0].response.status, proposalResponses[0].response.message,
 				proposalResponses[0].response.payload, proposalResponses[0].endorsement.signature));
 
 			// wait for the channel-based event hub to tell us that the
 			// instantiate transaction was committed on the peer
-			var promises = [];
+			let promises = [];
 			let event_hubs = channel.getChannelEventHubsForOrg();
 			logger.debug('found %s eventhubs for this organization %s',event_hubs.length, org_name);
 			event_hubs.forEach((eh) => {
@@ -107,7 +91,7 @@ var instantiateChaincode = async function(peers, channelName, chaincodeName, cha
 						let message = 'REQUEST_TIMEOUT:' + eh.getPeerAddr();
 						logger.error(message);
 						eh.disconnect();
-					}, 60000);
+					}, CONSTANTS.INSTANTIATION_TIMEOUT);
 					eh.registerTxEvent(deployId, (tx, code, block_num) => {
 						logger.info('The chaincode instantiate transaction has been committed on peer %s',eh.getPeerAddr());
 						logger.info('Transaction %s has status of %s in blocl %s', tx, code, block_num);
@@ -138,7 +122,7 @@ var instantiateChaincode = async function(peers, channelName, chaincodeName, cha
 				promises.push(instantiateEventPromise);
 			});
 
-			var orderer_request = {
+			let orderer_request = {
 				txId: tx_id, // must include the transaction id so that the outbound
 				             // transaction to the orderer will be signed by the admin
 							 // id as was the proposal above, notice that transactionID
@@ -147,7 +131,7 @@ var instantiateChaincode = async function(peers, channelName, chaincodeName, cha
 				proposalResponses: proposalResponses,
 				proposal: proposal
 			};
-			var sendPromise = channel.sendTransaction(orderer_request);
+			let sendPromise = channel.sendTransaction(orderer_request);
 			// put the send to the orderer last so that the events get registered and
 			// are ready for the orderering and committing
 			promises.push(sendPromise);
@@ -184,7 +168,7 @@ var instantiateChaincode = async function(peers, channelName, chaincodeName, cha
 
 	if (!error_message) {
 		let message = util.format(
-			'Successfully instantiate chaincode in organization %s to the channel \'%s\'',
+			'Successfully instantiated chaincode in organization %s to the channel %s',
 			org_name, channelName);
 		logger.info(message);
 		// build a response to send back to the REST caller
@@ -194,9 +178,13 @@ var instantiateChaincode = async function(peers, channelName, chaincodeName, cha
 		};
 		return response;
 	} else {
-		let message = util.format('Failed to instantiate. cause:%s',error_message);
+		let message = util.format('Failed to instantiate. cause: %s',error_message);
 		logger.error(message);
-		throw new Error(message);
+		let response = {
+			success: false,
+			message: message
+		};
+		return response;
 	}
 };
 exports.instantiateChaincode = instantiateChaincode;
--- tests/chaincode.test.js
+++ tests/chaincode.test.js
@@ -8,7 +8,25 @@ test("Request to install Chaincode to with valid parameters on " + CONSTANTS.ORG
   .post('/chaincodes')
   .set('Authorization', 'Bearer ' + CONSTANTS.ORG1_JWT)
   .send({
-	peers: ['peer0.org1.ela.com','peer1.org1.ela.com','peer0.org2.ela.com','peer1.org2.ela.com'],
+	peers: ['peer0.org1.ela.com','peer1.org1.ela.com'],
+	chaincodeName: CONSTANTS.CHAINCODE_NAME,
+	chaincodePath: CONSTANTS.CHAINCODE_PATH,
+	chaincodeType: CONSTANTS.CHAINCODE_TYPE,
+	chaincodeVersion: CONSTANTS.CHAINCODE_VERSION
+});
+  t.is(response.status, 200);
+  t.deepEqual(response.body, {
+    success: true,
+    message: "Successfully installed chaincode"
+  });
+});
+
+test("Request to install Chaincode to with valid parameters on " + CONSTANTS.ORG1_NAME, async t => {
+  const response = await request(app)
+  .post('/chaincodes')
+  .set('Authorization', 'Bearer ' + CONSTANTS.ORG1_JWT)
+  .send({
+	peers: ['peer0.org2.ela.com','peer1.org2.ela.com'],
 	chaincodeName: CONSTANTS.CHAINCODE_NAME,
 	chaincodePath: CONSTANTS.CHAINCODE_PATH,
 	chaincodeType: CONSTANTS.CHAINCODE_TYPE,
