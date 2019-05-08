5daa43e1f27c595d350b1cd506e6ed91891253ec
tittuvarghese <2311294+tittuvarghese@users.noreply.github.com>
2019-05-08 14:29:53 +0530

    Tet fixes

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
-const ORG1_NAME = "Org1"
-const ORG2_NAME = "Org2"
+exports.const ORG1_NAME = "Org1"
+exports.const ORG2_NAME = "Org2"
 
-const ORG1_EMAIL = "org1.ela.com"
-const ORG2_EMAIL = "org2.ela.com"
+exports.const ORG1_EMAIL = "org1.ela.com"
+exports.const ORG2_EMAIL = "org2.ela.com"
