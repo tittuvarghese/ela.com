fbf621d203787c609ac36d6c75fcb9d0a05f02dc
tittuvarghese <2311294+tittuvarghese@users.noreply.github.com>
2019-06-09 11:27:49 +0530

    Fixes

--- tests/e2e.js
+++ tests/e2e.js
@@ -1,7 +1,8 @@
 'use strict';
 require('./network.test.js');
 require('./identity.test.js');
-require('./user.test.js');
+// require('./user.test.js');
 require('./channel.test.js');
 require('./chaincode.test.js');
 require('./user_cc.test.js');
+require('./product_cc.test.js');
